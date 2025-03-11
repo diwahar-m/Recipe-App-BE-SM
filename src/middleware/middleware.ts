85f3884b9d0c039094e39c2af0a845f2fa50f31d
diwahar-m <msdiwahar5@gmail.com>
2025-03-11 16:06:12 +0530

    individual recipe get & delete routers added

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -12,19 +12,18 @@ export interface AuthRequest extends Request {
 export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
     const token = req.header('Authorization')?.replace('Bearer', "");
     if(!token){
-        return res.status(401).json({
+         res.status(401).json({
             success: false, 
             message: 'No valid token is provided! Authorization failed'
         })
     }
-
     try {
         const decoded = jwt.verify(token, 'JWT_SECRET') as DecodedToken;
         req.userId = decoded.userId;
         next();
 
     } catch(err){
-        res.status(401).json({
+         res.status(401).json({
             success: false, 
             message: 'Token is not a valid token, pls try again'
         })
--- src/routes/recipes.ts
+++ src/routes/recipes.ts
@@ -7,20 +7,104 @@ import Recipe from '../models/Recipe';
  const router = express.Router(); 
 
  router.post('/create', authMiddleware, async(req: AuthRequest, res: Response)=> {
-    const {title, description, difficulty} = req.body;
-    // 
-    const newlyCreatedRecipe = new Recipe({
-        title,
-        description, 
-        difficulty,
-        createdBy: req.userId,  
-    })
-    await newlyCreatedRecipe.save();
+    try {
 
-    res.status(200).json({
-        success: true,  
-        message: 'Recipe added successfully !'
-    })
+         const {title, description, difficulty} = req.body;
+         
+        const newlyCreatedRecipe = new Recipe({
+            title,
+            description, 
+            difficulty,
+            createdBy: req.userId,  
+        })
+        await newlyCreatedRecipe.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Recipe added successfully !',
+            data: newlyCreatedRecipe
+        })
+
+    } catch(err){
+        console.log(err); 
+        res.status(500).json({
+            success: false, 
+            message: 'Something went wrong! Please try again'
+        })
+    }
+ })
+
+ router.get('/get', authMiddleware, async(req: AuthRequest, res:Response)=> {
+    try {
+
+        const fetchAllRecipes = await Recipe.find({});
+
+        res.status(200).json({
+            success: true, 
+            data: fetchAllRecipes
+        })
+
+    } catch(err){
+        console.log(err); 
+        res.status(500).json({
+            success: false, 
+            message: 'Something went wrong! Please try again'
+        })
+    }
+ })
+ router.get('/get/:id', authMiddleware, async(req: AuthRequest, res:Response)=> {
+    try {
+
+        const getSingleRecipe = await Recipe.findOne({
+            _id: req.params.id,
+            // createdBy: req.userId
+        });
+        if(!getSingleRecipe){
+            return res.status(400).json({
+                success: false, 
+                message: 'Recipe not found!'
+            })
+        }
+        res.status(200).json({
+            success: true, 
+            message:'Recipe is fetchd successfully',
+            data: getSingleRecipe
+        })
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            success: false, 
+            message: 'Something went wrong! Please try again'
+        })
+    }
+ })
+
+ router.delete('/delete/:id',authMiddleware, async(req, res)=> {
+    try {
+
+        const getSingleRecipe = await Recipe.findOne({
+            _id: req.params.id,
+            //  createdBy: req.params.id
+        });
+        if(!getSingleRecipe){
+            return res.status(400).json({
+                success: false, 
+                message: `You don't have permission to delete the recipe `
+            })
+        }
+
+        await getSingleRecipe.deleteOne()
+        res.status(200).json({
+            success: true, 
+            message:'Recipe deleted successfully',
+        })
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            success: false, 
+            message: 'Something went wrong! Please try again'
+        })
+    }
  })
 
  export default router
